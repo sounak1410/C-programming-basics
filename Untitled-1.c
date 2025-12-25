6a6a3c09d42c39d5a3266c4d54907dec39b8fe9a
Ramesses II <dev.sounak2007@gmail.com>
2025-12-25 22:46:33 +0530

    Summing two numbers (input by users)

---
+++ Untitled-1.c
@@ -0,0 +1,11 @@
+
+int main()
+{ 
+    float n1, n2, n3;
+    printf("enter your first number: ");
+    scanf("%f", &n1);
+    printf("enter your second number: ");
+    scanf("%f", &n2);
+    n3 = n1 + n2;
+    printf("the sum of your entered values are: %f", n3);
+}
