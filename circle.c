a72c05487cf62e4e44490908beb7441aa79c1f52
Ramesses II <dev.sounak2007@gmail.com>
2026-01-08 01:49:31 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int r; float a, p; //r-radius, a-area, p-perimeter
+    printf("Enter the radius of the circle: ");
+    scanf("%d", &r);
+    a = 3.14 * pow(r, 2);
+    p = 2 * 3.14 * r;
+    printf("Area of the circle: %.2f \n", a);
+    printf("Perimeter of the circle: %.2f \n", p);
+    return 0;
+}
