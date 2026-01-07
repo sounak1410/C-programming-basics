94026140d15e8a97f0c98bfb8c94a8eecb7d7ef0
Ramesses II <dev.sounak2007@gmail.com>
2026-01-08 01:54:03 +0530

    area and perimeter of a circle
    This code helps in calculating the area and perimeter of any circle with its user input radius.

--- circle.c
+++ circle.c
@@ -1,3 +1,5 @@
+TITLE: Working with circles in C
+DESCRIPTION: This code helps in calculating the area and perimeter of any circle with its user input radius
 #include<stdio.h>
 #include<math.h>
 int main()
@@ -10,4 +12,5 @@ int main()
     printf("Area of the circle: %.2f \n", a);
     printf("Perimeter of the circle: %.2f \n", p);
     return 0;
+
 }
